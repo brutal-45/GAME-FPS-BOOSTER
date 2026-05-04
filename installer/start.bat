bd15ceaeaa00f1b4890d57937a41164322e4a146
brutal-45 <creatorsports81@gmail.com>
2026-05-04 19:47:23 +0530

    Update start.bat

--- installer/start.bat
+++ installer/start.bat
@@ -13,7 +13,7 @@ taskkill /f /im node.exe >nul 2>&1
 taskkill /f /im bun.exe >nul 2>&1
 
 :: Start the application
-start "" "http://localhost:3000"
+start "" "http://localhost:3000" 
 bun run server.js 
 
 exit
